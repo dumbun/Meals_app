4a5e0b7d9e339703795742832bf1dbcf20fd15f8
vamshi krishna <113350510+dumbun@users.noreply.github.com>
2023-04-10 22:52:04 +0530

    Day-2: add a stack and meal item page blur effect for the text on the images

--- lib/widgets/meal_iteam.dart
+++ lib/widgets/meal_iteam.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/models/meals.dart';
+import 'package:blur/blur.dart';
 
 class MealItem extends StatelessWidget {
   final title;
@@ -15,6 +16,40 @@ class MealItem extends StatelessWidget {
     @required this.complexity,
     @required this.affordability,
   });
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return "Simple";
+        break;
+      case Complexity.Hard:
+        return "Hard";
+        break;
+      case Complexity.Challenging:
+        return "Challenging";
+        break;
+      default:
+        return "Unknown";
+        break;
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return "Affordable";
+        break;
+      case Affordability.Luxurious:
+        return "Luxurious";
+        break;
+      case Affordability.Pricey:
+        return "Pricey";
+        break;
+      default:
+        return "Unknown";
+        break;
+    }
+  }
 
   void selectMeal() {}
 
@@ -45,9 +80,56 @@ class MealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
-                )
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Text(
+                    title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                      color: Colors.black,
+                    ),
+                    softWrap: true,
+                    overflow: TextOverflow.fade,
+                  ).frosted(
+                    blur: 15,
+                    borderRadius: BorderRadius.circular(20),
+                    padding: EdgeInsets.all(8),
+                  ),
+                ),
               ],
             ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.schedule),
+                      SizedBox(width: 6.0),
+                      Text('$duration min'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.work),
+                      SizedBox(width: 6.0),
+                      Text('$complexityText'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.currency_rupee_rounded),
+                      SizedBox(width: 6.0),
+                      Text('$affordabilityText'),
+                    ],
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
